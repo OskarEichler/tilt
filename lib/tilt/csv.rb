78b4d1b5a296df9007f2acb32a71e5a848aa8fd3
Oskar Eichler <62393985+OskarEichler@users.noreply.github.com>
2026-08-30 08:30:02 +0200

    Support nonliteral CSV options

--- lib/tilt/csv.rb
+++ lib/tilt/csv.rb
@@ -44,19 +44,88 @@ module Tilt
 
     def prepare
       @outvar = @options.delete(:outvar) || '_csvout'
+      @literal_options = literal_options?(@options)
+      unless @literal_options
+        @csv_local = "__tilt_csv_#{object_id.abs}"
+        if @fixed_locals
+          @csv_fixed_locals = true
+          fixed_locals = @fixed_locals[1...-1]
+          fixed_locals = ", #{fixed_locals}" unless fixed_locals.empty?
+          @fixed_locals = "(#{@csv_local}#{fixed_locals})"
+        end
+      end
     end
 
     def precompiled_template(locals)
-      <<-RUBY
+      if @literal_options
+        <<-RUBY
         #{@outvar} = CSV.generate(**#{@options}) do |csv|
           #{@data}
         end
       RUBY
+      else
+        <<-RUBY
+        csv = #{@csv_local}
+        #{@data}
+        #{@outvar} = #{@csv_local}.string
+      RUBY
+      end
     end
 
     def precompiled(locals)
       source, offset = super
       [source, offset + 1]
+    end
+
+    def compiled_method(local_keys, scope_class=nil)
+      unless @literal_options || @csv_fixed_locals
+        local_keys = local_keys.dup
+        local_keys << @csv_local.to_sym
+      end
+      super(local_keys, scope_class)
+    end
+
+    private
+
+    def compile_template_method(local_keys, scope_class=nil)
+      method = super
+      return method if @literal_options
+
+      options = @options.dup.freeze
+      csv_local = @csv_local.to_sym
+      wrapper = Module.new
+      if @csv_fixed_locals
+        wrapper.define_method(:render) do |*args, **locals, &block|
+          CSV.generate(**options) do |csv|
+            bound_method = method.bind(self)
+            if locals.empty?
+              bound_method.call(csv, *args, &block)
+            else
+              bound_method.call(csv, *args, **locals, &block)
+            end
+          end
+        end
+      else
+        wrapper.define_method(:render) do |locals, &block|
+          CSV.generate(**options) do |csv|
+            method.bind(self).call(locals.merge(csv_local => csv), &block)
+          end
+        end
+      end
+      wrapper.instance_method(:render)
+    end
+
+    def literal_options?(value)
+      case value
+      when nil, true, false, Symbol, String, Integer, Float
+        true
+      when Array
+        value.all?{|entry| literal_options?(entry)}
+      when Hash
+        !value.default_proc && value.all?{|key, entry| literal_options?(key) && literal_options?(entry)}
+      else
+        false
+      end
     end
 
   end
